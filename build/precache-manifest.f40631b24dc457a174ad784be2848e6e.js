2f9d233075cf70593d7e6412e3b427913eb8c5bb
nifacodes <nifacodes@gmail.com>
2020-01-22 15:47:12 -0500

    create build

---
+++ build/precache-manifest.f40631b24dc457a174ad784be2848e6e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f606ffe3a88c09d048de18997d9f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181f3f406b84013a15dc",
+    "url": "/static/css/main.8d1e310a.chunk.css"
+  },
+  {
+    "revision": "2915ec9c7a9d60b98f5c",
+    "url": "/static/js/2.fe15b0df.chunk.js"
+  },
+  {
+    "revision": "4544b2b45c9cfea0feb3c391ccb163d8",
+    "url": "/static/js/2.fe15b0df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "181f3f406b84013a15dc",
+    "url": "/static/js/main.a9b2257d.chunk.js"
+  },
+  {
+    "revision": "77b9352164356c5f93f5",
+    "url": "/static/js/runtime-main.af022d2b.js"
+  },
+  {
+    "revision": "be86d7a1d4bffe88af1ecd30e8c794f5",
+    "url": "/static/media/1.be86d7a1.png"
+  },
+  {
+    "revision": "53b4d42f2ac05148cf20c7d997aa8f48",
+    "url": "/static/media/2.53b4d42f.png"
+  },
+  {
+    "revision": "13735280fdb42a12b87e9be62199d6b2",
+    "url": "/static/media/3.13735280.png"
+  },
+  {
+    "revision": "58af08ba14645a1a7eadaebac4aa654f",
+    "url": "/static/media/4.58af08ba.png"
+  },
+  {
+    "revision": "727f5488e5b64b98c1412ebb012bbc13",
+    "url": "/static/media/bg-nophone.727f5488.png"
+  },
+  {
+    "revision": "62507078fd69e6edd15f0d3532b8d0f3",
+    "url": "/static/media/folded_paper.62507078.png"
+  },
+  {
+    "revision": "d768fd81a81875d055a4068731864706",
+    "url": "/static/media/paper.d768fd81.png"
+  },
+  {
+    "revision": "33da35c130cd09798b7bf817923b9718",
+    "url": "/static/media/phone2.33da35c1.png"
+  }
+]);
